7d0abdb2b5ba8733bf956949453447dd9cf16805
Pheonix <arshi900389@gmail.com>
2025-12-07 11:03:29 +0530

    24.11.25

---
+++ November Streak Question/LC-1018.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int num = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            num = (num * 2 + nums[i]) % 5; // Build the number modulo 5
+            result.add(num == 0); // Check if divisible by 5
+        }
+        
+        return result;
+    }
+}
