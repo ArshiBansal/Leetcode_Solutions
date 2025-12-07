faebab66096d94571d28e5f1cdf4dfed6de5ccaa
Pheonix <arshi900389@gmail.com>
2025-12-07 11:00:48 +0530

    23.11.25

---
+++ November Streak Question/LC-1262.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSumDivThree(int[] nums) {
+        // dp[i] will store the maximum sum with remainder i when divided by 3
+        int[] dp = new int[3];
+        Arrays.fill(dp, 0);
+
+        for (int num : nums) {
+            int[] temp = dp.clone(); // use a temporary array to store updates
+            for (int i = 0; i < 3; i++) {
+                int sum = dp[i] + num;
+                temp[sum % 3] = Math.max(temp[sum % 3], sum);
+            }
+            dp = temp; // update dp
+        }
+
+        return dp[0]; // max sum divisible by 3
+    }
+}
