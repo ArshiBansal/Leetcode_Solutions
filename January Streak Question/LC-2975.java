e7b900cebb2dec1e43ae06b8f740e8e9b6c03c2c
Pheonix <arshi900389@gmail.com>
2026-01-17 13:54:54 +0530

    16.01.26

---
+++ January Streak Question/LC-2975.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        final long MOD = 1_000_000_007L;
+
+        int[] h = new int[hFences.length + 2];
+        int[] v = new int[vFences.length + 2];
+
+        h[0] = 1;
+        h[h.length - 1] = m;
+        for (int i = 0; i < hFences.length; i++) h[i + 1] = hFences[i];
+
+        v[0] = 1;
+        v[v.length - 1] = n;
+        for (int i = 0; i < vFences.length; i++) v[i + 1] = vFences[i];
+
+        Arrays.sort(h);
+        Arrays.sort(v);
+
+        Set<Integer> horizontalDistances = new HashSet<>();
+        for (int i = 0; i < h.length; i++) {
+            for (int j = i + 1; j < h.length; j++) {
+                horizontalDistances.add(h[j] - h[i]);
+            }
+        }
+
+        long maxSide = -1;
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < v.length; j++) {
+                int dist = v[j] - v[i];
+                if (horizontalDistances.contains(dist)) {
+                    maxSide = Math.max(maxSide, dist);
+                }
+            }
+        }
+
+        if (maxSide == -1) return -1;
+        return (int)((maxSide * maxSide) % MOD);
+    }
+}
