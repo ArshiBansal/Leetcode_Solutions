fbfc45d0be2174ee0cc23230f154de2fa0128c38
Pheonix <150040105+ArshiBansal@users.noreply.github.com>
2026-03-31 12:46:12 +0530

    29-03-26

---
+++ March Streak Question/LC-2839.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        String group1_s1 = "" + s1.charAt(0) + s1.charAt(2);
+        String group1_s2 = "" + s2.charAt(0) + s2.charAt(2);
+        String group2_s1 = "" + s1.charAt(1) + s1.charAt(3);
+        String group2_s2 = "" + s2.charAt(1) + s2.charAt(3);
+        
+        return (sorted(group1_s1).equals(sorted(group1_s2)) && sorted(group2_s1).equals(sorted(group2_s2)));
+    }
+
+    private String sorted(String s) {
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
