bfd83ab6def97674d8e28d3981c7eafa70044f49
Pheonix <150040105+ArshiBansal@users.noreply.github.com>
2026-04-07 18:27:16 +0530

    06.04.26

---
+++ April Streak Question/LC-874.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // दिशा: North, East, South, West
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int dir = 0; // start facing North
+        
+        // obstacles को HashSet में store करो
+        Set<String> obstacleSet = new HashSet<>();
+        for (int[] obs : obstacles) {
+            obstacleSet.add(obs[0] + "," + obs[1]);
+        }
+        
+        int x = 0, y = 0;
+        int maxDist = 0;
+        
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                // turn right
+                dir = (dir + 1) % 4;
+            } else if (cmd == -2) {
+                // turn left
+                dir = (dir + 3) % 4;
+            } else {
+                // move forward step by step
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
+                    
+                    // obstacle check
+                    if (obstacleSet.contains(nx + "," + ny)) {
+                        break;
+                    }
+                    
+                    x = nx;
+                    y = ny;
+                    
+                    maxDist = Math.max(maxDist, x*x + y*y);
+                }
+            }
+        }
+        
+        return maxDist;
+    }
+}
